54b8394ce8c2bcded9885cbc44556e5d1c910ddb
Vladislab Golubinov <vlad.vdlald@gmail.com>
2019-12-18 22:46:52 +0300

    The shortest way to solve a mnemonic problem

--- src/main/java/vladislav/problems/Mnemonics.java
+++ src/main/java/vladislav/problems/Mnemonics.java
@@ -1,11 +1,16 @@
 package vladislav.problems;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Mnemonics {
     static String replace(Stream<String> stream, String replacement) {
-        StringBuilder string = new StringBuilder();
-        stream.forEach(s -> string.append(s.replaceAll("&[^\\s;]+;", replacement)));
-        return string.toString();
+        // variant 1: speed 44ms - 49ms for mnemonics.md
+//        StringBuilder string = new StringBuilder();
+//        stream.forEach(s -> string.append(s.replaceAll("&[^\\s;]+;", replacement)));
+//        return string.toString();
+
+        // variant 2: speed 74ms - 90ms for mnemonics.md
+        return stream.map(s -> s.replaceAll("&[^\\s;]+;", replacement)).reduce((s1, s2) -> s1 + s2).get();
     }
 }
