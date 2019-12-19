0454be719ea9af04222b4bad127f57ed7bc6db6e
Vladislav Golubinov <vlad.vdlald@gmail.com>
2019-12-19 09:45:46 +0300

    Added performance testing for all mnemonic methods

--- src/main/java/vladislav/problems/Mnemonics.java
+++ src/main/java/vladislav/problems/Mnemonics.java
@@ -5,36 +5,40 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class Mnemonics {
-    static String replace(Stream<String> stream, String replacement) {
-        // variant 1: speed 44ms - 49ms for mnemonics.md
-//        StringBuilder string = new StringBuilder();
-//        stream.forEach(s -> string.append(s.replaceAll("&[^\\s;]+;", replacement)));
-//        return string.toString();
+public class Mnemonics {
+    public static String replace(Stream<String> stream, String replacement) {
+        return stream.map(s -> s.replaceAll("&[a-zA-Z0-9]+;", replacement)).reduce(String::concat).get();
+    }
 
-        // variant 2: speed 74ms - 90ms for mnemonics.md
-//        return stream.map(s -> s.replaceAll("&[^\\s;]+;", replacement)).reduce((s1, s2) -> s1. + s2).get();
+    public static String replace1(Stream<String> stream, String replacement) {
+        StringBuilder string = new StringBuilder();
+        stream.forEach(s -> string.append(s.replaceAll("&[a-zA-Z0-9]+;", replacement)));
+        return string.toString();
+    }
 
-        // variant 3: speed 29ms - 39ms for mnemonics.md
-        return stream.map(s -> s.replaceAll("&[a-zA-Z]+;", replacement)).reduce(String::concat).get();
+    public static String replace2(Stream<String> stream, String replacement) {
+        return stream.map(s -> s.replaceAll("&[a-zA-Z0-9]+;", replacement)).reduce((s1, s2) -> s1 + s2).get();
+    }
 
-        // variant 4: speed 32ms - 43ms for mnemonics.md
-//        return stream.map(s -> s.replaceAll("&[a-zA-Z]+;", replacement)).collect(Collectors.joining());
+    public static String replace3(Stream<String> stream, String replacement) {
+        return stream.map(s -> s.replaceAll("&[a-zA-Z0-9]+;", replacement)).collect(Collectors.joining());
+    }
 
-        // variant 5: speed 29ms - 36ms for mnemonics.md
-//        return stream.map(s -> s.replaceAll("&[a-zA-Z]+;", replacement)).collect(Collector.of(
-//                () -> new StringJoiner(""),
-//                StringJoiner::add,
-//                StringJoiner::merge,
-//                StringJoiner::toString
-//        ));
+    public static String replace4(Stream<String> stream, String replacement) {
+        return stream.map(s -> s.replaceAll("&[a-zA-Z0-9]+;", replacement)).collect(Collector.of(
+                () -> new StringJoiner(""),
+                StringJoiner::add,
+                StringJoiner::merge,
+                StringJoiner::toString
+        ));
+    }
 
-        // variant 6: speed 30ms - 39ms (max: 54ms) for mnemonics.md
-//        return stream.map(s -> s.replaceAll("&[a-zA-Z]+;", replacement)).collect(Collector.of(
-//                StringBuilder::new,
-//                StringBuilder::append,
-//                StringBuilder::append,
-//                StringBuilder::toString
-//        ));
+    public static String replace5(Stream<String> stream, String replacement) {
+        return stream.map(s -> s.replaceAll("&[a-zA-Z0-9]+;", replacement)).collect(Collector.of(
+                StringBuilder::new,
+                StringBuilder::append,
+                StringBuilder::append,
+                StringBuilder::toString
+        ));
     }
 }
---
+++ src/main/java/vladislav/problems/benchmark/BenchmarkMnemonics.java
@@ -0,0 +1,76 @@
+package vladislav.problems.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import vladislav.problems.Mnemonics;
+
+import java.util.stream.Stream;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+@BenchmarkMode(Mode.Throughput)
+@State(Scope.Benchmark)
+@OutputTimeUnit(MILLISECONDS)
+@Warmup(iterations = 5, time = 1000, timeUnit = MILLISECONDS)
+@Measurement(iterations = 10, time = 1000, timeUnit = MILLISECONDS)
+@Fork(1)
+public class BenchmarkMnemonics {
+
+    @Param({"10"})
+    private int N;
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(BenchmarkMnemonics.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void replace(Blackhole bh) {
+        bh.consume(Mnemonics.replace(createData(), "\"\n"));
+    }
+
+    @Benchmark
+    public void replace1(Blackhole bh) {
+        bh.consume(Mnemonics.replace1(createData(), "\"\n"));
+    }
+
+    @Benchmark
+    public void replace2(Blackhole bh) {
+        bh.consume(Mnemonics.replace2(createData(), "\"\n"));
+    }
+
+    @Benchmark
+    public void replace3(Blackhole bh) {
+        bh.consume(Mnemonics.replace3(createData(), "\"\n"));
+    }
+
+    @Benchmark
+    public void replace4(Blackhole bh) {
+        bh.consume(Mnemonics.replace4(createData(), "\"\n"));
+    }
+
+    @Benchmark
+    public void replace5(Blackhole bh) {
+        bh.consume(Mnemonics.replace5(createData(), "\"\n"));
+    }
+
+    private Stream<String> createData() {
+        Stream.Builder<String> stream;
+        stream = Stream.<String>builder();
+        for (int i = 0; i < N; i++) {
+            stream.add(String.format("&quot%d;", i));
+        }
+        return stream.build();
+    }
+
+}
+
